80dcbafb979059562f3f72c95f735aaf102c881d
luk <lukasz.osadik@wp.pl>
2025-05-16 18:48:51 +0200

    add ascii logo

---
+++ ddterm.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+printf "$(cat <<EOF
+\e[0;32m
+                 ww            ww
+                IWWI          IWWI
+                IWWI          IWWI
+                IWWI          IWWI
+ ww             IWWI          IWWI
+IWWX            IWWI          IWWI
+ XWWX           IWWI          IWWI
+  XWWX    xwwwwwWWWI    xwwwwwWwwI
+  XWWX   XWWprnqWWWI   XWWprnqWWWI
+ XWWX   IWWi    lWWI  IWWi    lWWI
+XWWX    IWWI    IWWI  IWWI    IWWI
+ww'     IWWl    iWWI  IWWl    iWWI
+         XWWwvvwWWWI   XWWwvvwWWWI
+          lWWWWWNWWD    lWWWWWNWWD
+
+
+\e[0m
+EOF
+)"
